e64c78653ccabfbef8d2462d7d568b9bd9eae558
Divyansh Yaduvanshi <divyanshyaduvanshi2004@gmail.com>
2025-06-17 22:54:54 +0530

    init - added animated background

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Background from "./components/Background";
+
+function App() {
+  return (
+    <div className="flex">
+      <Background />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Background.tsx
@@ -0,0 +1,11 @@
+import HeroGeometric from "./kokonutui/hero-geometric";
+
+const Background = () => {
+  return (
+    <div className="flex w-full h-screen items-center justify-center bg-[#030303] overflow-hidden">
+      <HeroGeometric />
+    </div>
+  );
+};
+
+export default Background;
